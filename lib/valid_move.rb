e4c1f32ac793f91538c874b6ff70b6af0d4be8ac
Chidubem Ogbudibe <ogbudibe@mail.utoronto.ca>
2019-10-18 01:48:10 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,14 @@
-# code your #valid_move? method here
+def valid_move(board, index)
+  if(1 <= index <= 9 && position_taken?(board, index))
+    return true
+  end
+end
 
-
+def position_taken?(board, index)
+  if(board[index] == " " || board[index] == "" || board[index] == nil)
+    return false
+  else
+    return true
+  end
+end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
